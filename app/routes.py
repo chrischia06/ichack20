c08bc35b404eba22acc0d121aa180c552afa646d
Chris Chia <chrischia06@gmail.com>
2020-02-08 15:16:24 +0000

    update with john hopkins dashboard

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app import app
 import requests
 
 # from app.forms import SubmitForm
+@app.route('/statistics')
 @app.route('/')
 def index():
 	r = requests.get("https://coronavirus.m.pipedream.net/")
@@ -12,6 +13,13 @@ def index():
 	for i in range(len(data['rawData'])):
 		data['rawData'][i]['Province'] = data['rawData'][i]['Province/State']
 		data['rawData'][i]['Country'] = data['rawData'][i]['Country/Region']
-
 	return render_template("index.html", data = data)
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/john_hopkins')
+def john_hopkins():
+	return render_template("john_hopkins.html")
 
